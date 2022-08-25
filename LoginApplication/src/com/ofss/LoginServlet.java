e26465b237481a2cb2d63a332f6bf5eba0634988
Guru <gurumurthy.ramamurthy@gmail.com>
2022-08-25 15:00:39 +0530

    Implemented Login Functionality

---
+++ LoginApplication/src/com/ofss/LoginServlet.java
@@ -0,0 +1,51 @@
+package com.ofss;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter ps=response.getWriter();
+		String un=request.getParameter("un");
+		String pw=request.getParameter("pw");
+		if (un.equals("oracle") && pw.equals("oracle123"))
+		{
+			ps.println("You are a valid user");
+		}
+		else
+		{
+			ps.println("You are an invalid user");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
